3fea0b5f6eb66ee27374953180e2d4463036d7b5
lyc <lyc_0@mail.nwpu.edu.cn>
2022-12-23 17:27:17 +0800

    change homepage in package.json

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 // Style
 import navbarStyle from './Navbar.module.css';
@@ -10,6 +10,8 @@ import list from '../../res/list.svg';
 
 function Navbar(props) {
     const navTo = useNavigate();
+    const location = useLocation();
+    const filename = location.pathname.split('/')[2];
 
     function toLibrary(ev) {
         navTo('/');
@@ -18,8 +20,8 @@ function Navbar(props) {
         <div className={navbarStyle.navbar}>
             <img alt='' src={list} draggable={false} onClick={toLibrary}></img>
             <div className={navbarStyle.title}>
-                <span className={navbarStyle.folderName}>folder/</span>
-                <span className={navbarStyle.fileName}>filename</span>
+                <span className={navbarStyle.folderName}>我的设计/</span>
+                <span className={navbarStyle.fileName}>{filename}</span>
             </div>
         </div>
     );
